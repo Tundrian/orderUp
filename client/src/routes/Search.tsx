d1dd2eea192db315152d404e7aebf9b92477fd45
Antonio <antoniosansotta@gmail.com>
2022-11-22 22:40:32 -0500

    fixed env file access

--- client/src/routes/Search.tsx
+++ client/src/routes/Search.tsx
@@ -6,47 +6,6 @@ interface DataFace {
     image: string,
 }
 
-// const data: DataFace[] = [
-//     {
-//         id: '1',
-//         recipeID: '0001',
-//         recipeName: 'test001',
-//         imageURL: 'www.url.com',
-//         mealtime: 'lunch'
-//     }, {
-//         id: '2',
-//         recipeID: '0002',
-//         recipeName: 'test002',
-//         imageURL: 'www.url.com',
-//         mealtime: 'lunch'
-//     }, {
-//         id: '3',
-//         recipeID: '0003',
-//         recipeName: 'test003',
-//         imageURL: 'www.url.com',
-//         mealtime: 'lunch'
-//     },
-//     {
-//         id: '4',
-//         recipeID: '0004',
-//         recipeName: 'test004',
-//         imageURL: 'www.url.com',
-//         mealtime: 'lunch'
-//     }, {
-//         id: '5',
-//         recipeID: '0005',
-//         recipeName: 'test005',
-//         imageURL: 'www.url.com',
-//         mealtime: 'lunch'
-//     }, {
-//         id: '6',
-//         recipeID: '0006',
-//         recipeName: 'test006',
-//         imageURL: 'www.url.com',
-//         mealtime: 'lunch'
-//     },
-// ]
-
 interface Recipe {
     id: string,
     title: string,
@@ -56,34 +15,12 @@ interface Recipe {
 
 function Search() {
 
-    // const [popular, setPopular] = useState<[any]>([''])
     const [search, setSearch] = useState<string>('')
     const [sRecipes, setSRecipes] = useState<[Recipe] | []>([])
 
-    useEffect(() => {
-        // getPopular()
-    }, [])
-
-    // const getPopular = async () => {
-
-    //     const check: string= localStorage.getItem('popular') || ''
-    
-    //     if(check !== ''){
-    //       setPopular(JSON.parse(check))
-    //     }else{
-    //       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-    //       const data = await api.json()
-    //       localStorage.setItem('popular', JSON.stringify(data.recipes))
-    //       console.log("recipes: ", data.recipes)
-    //       setPopular(data.recipes)
-    //     }
-    
-    //     console.log(popular)
-    //   }
-
       const formSubmit = async (e:React.SyntheticEvent) => {
         e.preventDefault()
-        const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${search}&number=9`)
+        const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_API_KEY}&query=${search}&number=9`)
           const data = await api.json()
           console.log('data: ', data.results)
           setSRecipes(data.results)
@@ -97,12 +34,11 @@ function Search() {
     return (
         <>
             <div className="mt-20 mx-10">
-                <form className="form-control" onSubmit={formSubmit}>
-                    <input className="input input-bordered" value={search} onChange={handleSearchChange} type="text" placeholder="Search" />
-                    <button className="rounded-md py-1 px-4 bg-lime-300 mx-5" type="submit">Search</button>
+                <form className="form-control flex flex-row flex-nowrap" onSubmit={formSubmit}>
+                    <input className="input input-bordered w-[80%]" value={search} onChange={handleSearchChange} type="text" placeholder="Search" />
+                    <button className="rounded-md py-2 px-4 bg-red-800 mx-2 w-[20%]" type="submit">Search</button>
                 </form>
                 <div className="my-10 grid grid-cols-3 gap-10">
-                    {/* {data.length && data.map(x =>  */}
                     {sRecipes.length && sRecipes.map(x => 
                         (
                             <div key={x.id} className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -111,7 +47,7 @@ function Search() {
                                     <h2 className="card-title">{x.title}</h2>
                                     <p>{x.summary}</p>
                                     <div className="card-actions justify-end">
-                                        <button className="btn btn-primary">Buy Now</button>
+                                        <button className="btn btn-primary border-none bg-red-800">Buy Now</button>
                                     </div>
                                 </div>
                             </div>
