528268db1427aedf6bfff87ea86c5879a9f1b5f8
Antonio <antoniosansotta@gmail.com>
2022-11-29 21:40:37 -0500

    added get recipes from server on protected page

--- client/src/routes/Protected.tsx
+++ client/src/routes/Protected.tsx
@@ -1,19 +1,43 @@
 import { useNavigate } from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+interface Recipe {
+  id: string,     //recipeID
+  title: string,  //recipeName
+  image: string,  //imageURL
+  summary: string //mealtime
+}
 
 function Protected() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const { user } = useSelector((state: any) => state.auth)
-
+  const [sRecipes, setSRecipes] = useState<[Recipe] | []>([])
 
   useEffect(() => {
     console.log(user)
     if(!user){
       navigate('/home')
     }
+    loadRecipes()
+
   }, [user, navigate, dispatch, ])
+
+  const loadRecipes = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/recipe", {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${user.token}`
+        },
+      })
+      await console.log(response.json())
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div>Protected</div>
